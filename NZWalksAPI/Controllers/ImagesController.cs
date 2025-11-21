0e4545e28082194baa2bb247d1ab6b7297e7c740
Srikanta Nayak <nayaksrikanta11@gmail.com>
2025-11-21 18:36:25 +0530

    Image Upload Functionality Added

---
+++ NZWalksAPI/Controllers/ImagesController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Models.Domain;
+using NZWalksAPI.Models.DTO;
+using NZWalksAPI.Repositories;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto request)
+        {
+            ValidateFileUpload(request);
+            if (ModelState.IsValid)
+            {
+                //Convert Dto to Domain Model
+                var imageDomainModel = new Image
+                {
+                    File = request.File,
+                    FileExtension = Path.GetExtension(request.File.FileName),
+                    FileSizeInBytes = request.File.Length,
+                    FileName = request.FileName,
+                    FileDescription = request.FileDescription,
+
+                };
+
+                await imageRepository.Upload(imageDomainModel);
+                return Ok(imageDomainModel);
+            }
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto request)
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+            if(!allowedExtensions.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsupported file Extension");
+            }
+            if(request.FileName.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File Size more than 10MB, Please upload a smaller size file");
+            }
+        }
+    }
+}
